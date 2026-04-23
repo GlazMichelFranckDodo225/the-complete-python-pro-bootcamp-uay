36d8c600e4eb2d625c66a7104319d4dc40b6785f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-04-23 06:17:15 +0200

    String Manipulation and Code Intelligence

---
+++ 2_String_Manipulation_and_Code_Intelligence.py
@@ -0,0 +1,8 @@
+# String Manipulation and Code Intelligence
+# Back "n" character (\n)
+print("Hello World.\nHello world.\nHello world.")
+
+# Concatenation
+print("Hello" + " Glaz.")
+print("Hello " + "Michael.")
+print("Hello" + " " + "Franck")
