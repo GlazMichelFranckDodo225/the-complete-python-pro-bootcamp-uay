620fd5140f9bf369e3afa1a7d039317618ac4519
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-04-24 12:28:18 +0200

    Python Variables

---
+++ 4_Python_Variables.py
@@ -0,0 +1,24 @@
+# Python Variables
+# input("What is your name?")
+# name = input("What is your name?")
+# print("Hi, " + name + "!")
+
+# name = input("What is your name?")
+# print("Your name consists of " + str(len(name)) + " characters.")
+
+# name = input("Enter your name: ")
+# length = int(len(name))
+# print("Your name contains : " + str(length) + " characters")
+
+# Switch the contents of 2 variables
+glass1 = "milk"
+glass2 = "juice"
+print("Glass 1 contains : " + glass1)
+print("Glass 2 contains : " + glass2)
+temp = glass1
+glass1 = glass2
+glass2 = temp
+print("Glass 1 contains : " + glass1)
+print("Glass 2 contains : " + glass2)
+
+
